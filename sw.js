dc5583fef3de444ba0596c6a41df586d436a74a9
maxzxcv <fgh123456789.com@gmail.com>
2026-05-16 11:27:46 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'repair-stock-v1';
+const ASSETS = [
+  'index.html',
+  'Search.html',
+  'add_Item.html',
+  'view_category.html',
+  'Logodrive.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
